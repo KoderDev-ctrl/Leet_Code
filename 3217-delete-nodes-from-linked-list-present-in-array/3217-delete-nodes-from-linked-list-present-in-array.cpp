df28ad47e97505ef96e4b9c41e9243509f07ef42
KoderDev-ctrl <devprofessional695@gmail.com>
2026-07-29 22:06:45 +0530

    Time: 27 ms (97.13%), Space: 209.4 MB (100%) - LeetHub

---
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> mpp(nums.begin(), nums.end());
+
+        while (head && mpp.count(head->val))
+            head = head->next;
+
+        ListNode* curr = head;
+        while (curr && curr->next) {
+            while (curr->next && mpp.count(curr->next->val)) {
+                curr->next = curr->next->next;
+            }
+            curr = curr->next;
+        }
+        return head;
+    }
+};
+const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUFFER_SIZE]; size_t buffer_pos = 0; void* operator new(size_t size) { constexpr std::size_t alignment = alignof(std::max_align_t); size_t padding = (alignment - (buffer_pos % alignment)) % alignment; size_t total_size = size + padding; char* aligned_ptr = &buffer[buffer_pos + padding]; buffer_pos += total_size; return aligned_ptr; } void operator delete(void* ptr, unsigned long) {} void operator delete(void* ptr) {} void operator delete[](void* ptr) {}
