ace37392fa5d492ecfa75f08e429fc9ca7abd0d1
KoderDev-ctrl <devprofessional695@gmail.com>
2026-08-06 17:02:38 +0530

    Time: 0 ms (100%), Space: 7.8 MB (100%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char ch : s) {
+            if (!st.empty() && st.top() == '(' && ch == ')') {
+                st.pop();
+            }
+            else if (!st.empty() && st.top() == '[' && ch == ']') {
+                st.pop();
+            }
+            else if (!st.empty() && st.top() == '{' && ch == '}') {
+                st.pop();
+            }
+            else {
+                st.push(ch);
+            }
+        }
+
+        return st.empty();
+    }
+};
+const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUFFER_SIZE]; size_t buffer_pos = 0; void* operator new(size_t size) { constexpr std::size_t alignment = alignof(std::max_align_t); size_t padding = (alignment - (buffer_pos % alignment)) % alignment; size_t total_size = size + padding; char* aligned_ptr = &buffer[buffer_pos + padding]; buffer_pos += total_size; return aligned_ptr; } void operator delete(void* ptr, unsigned long) {} void operator delete(void* ptr) {} void operator delete[](void* ptr) {}
