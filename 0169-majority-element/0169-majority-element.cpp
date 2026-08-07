4420704321fc286710d0e68bb8ed10ac0c4eb95e
KoderDev-ctrl <devprofessional695@gmail.com>
2026-08-07 15:26:54 +0530

    Time: 0 ms (100%), Space: 27.2 MB (99.97%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>u;
+        for(int i=0;i<nums.size();i++){
+            u[nums[i]]+=1;
+        }
+        int v;
+        for(auto it =u.begin();it!=u.end();it++){
+            if(it->second >(nums.size()/2)){
+               v= it->first;
+            }
+        }
+        return v;
+    }
+};
+const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUFFER_SIZE]; size_t buffer_pos = 0; void* operator new(size_t size) { constexpr std::size_t alignment = alignof(std::max_align_t); size_t padding = (alignment - (buffer_pos % alignment)) % alignment; size_t total_size = size + padding; char* aligned_ptr = &buffer[buffer_pos + padding]; buffer_pos += total_size; return aligned_ptr; } void operator delete(void* ptr, unsigned long) {} void operator delete(void* ptr) {} void operator delete[](void* ptr) {}
