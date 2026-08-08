d03c9c65c53afeb52abfbc429952b9d7464f5dab
KoderDev-ctrl <devprofessional695@gmail.com>
2026-08-08 14:29:15 +0530

    Time: 0 ms (100%), Space: 7.9 MB (99.96%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int x=0;
+        for(int i=0;i<s.length();i++){
+            x=x^(s[i]-'a')^(t[i]-'a');
+        }
+        x=x^(t[t.length()-1]-'a');
+        return char(x+'a');
+    }
+};
+const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUFFER_SIZE]; size_t buffer_pos = 0; void* operator new(size_t size) { constexpr std::size_t alignment = alignof(std::max_align_t); size_t padding = (alignment - (buffer_pos % alignment)) % alignment; size_t total_size = size + padding; char* aligned_ptr = &buffer[buffer_pos + padding]; buffer_pos += total_size; return aligned_ptr; } void operator delete(void* ptr, unsigned long) {} void operator delete(void* ptr) {} void operator delete[](void* ptr) {}
