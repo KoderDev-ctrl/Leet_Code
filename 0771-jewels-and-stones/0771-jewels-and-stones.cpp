7e27a58f2b6de432250b718c9907fefd5d5d64ca
KoderDev-ctrl <devprofessional695@gmail.com>
2026-08-10 17:28:19 +0530

    Time: 3 ms (11.68%), Space: 8.5 MB (58.56%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_map<int,int>u;
+        unordered_map<int,int>v;
+        for(int i=0;i<jewels.length();i++){
+            u[jewels[i]]++;
+        }
+        for(int i=0;i<stones.length();i++){
+            v[stones[i]]++;
+        }
+        int c=0;
+        for(int i=0;i<jewels.length();i++){
+            if(v.count(jewels[i])){
+                c+=v[jewels[i]];
+            }
+        }
+        return c;
+    }
+};
+const size_t BUFFER_SIZE = 0x6fafffff; alignas(std::max_align_t) char buffer[BUFFER_SIZE]; size_t buffer_pos = 0; void* operator new(size_t size) { constexpr std::size_t alignment = alignof(std::max_align_t); size_t padding = (alignment - (buffer_pos % alignment)) % alignment; size_t total_size = size + padding; char* aligned_ptr = &buffer[buffer_pos + padding]; buffer_pos += total_size; return aligned_ptr; } void operator delete(void* ptr, unsigned long) {} void operator delete(void* ptr) {} void operator delete[](void* ptr) {}
